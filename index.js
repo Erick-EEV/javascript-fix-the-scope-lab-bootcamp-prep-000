a573bc6b453087665097ccc01f098b6ecfd4196f
Erick Villeda <eeverickeev@gmail.com>
2020-07-24 23:57:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ function add2(n) {
   const two = 2
 }
 
-var funkyFunction = function() {
+var funkyFunction = "FUNKY!"; {
   return function() {
-    var theFunk = "FUNKY!"
     return "FUNKY!"
   }
 }
